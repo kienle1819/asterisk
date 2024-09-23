41775643dfda205686cf68eb39d3da9c7499b1e3
Kien Le <tonykien2012@gmail.com>
2024-09-23 22:08:23 +0700

    Update scripts-ast20.sh

--- scripts-ast20.sh
+++ scripts-ast20.sh
@@ -1,62 +1,66 @@
 #!/bin/bash
 ###############################################################################
-#Script Name    : script asterisk 20                       
-#Description    : Scripts cài đặt asterisk trên Ubuntu & Debian              
-#Author         : Mr.Kien Le    
+# Script Name    : script_asterisk_20                       
+# Description    : Script to install Asterisk 20 on Ubuntu & Debian              
+# Author         : Mr.Kien Le    
 ################################################################################
 
-#Update system & reboot
+# Update system & reboot
 sudo apt update && sudo apt -y upgrade
-sudo reboot
 
-#Install Asterisk 20 LTS dependencies
-sudo apt -y install git curl wget libnewt-dev libssl-dev libncurses5-dev subversion  libsqlite3-dev build-essential libjansson-dev libxml2-dev  uuid-dev
+echo "System updated. Reboot is recommended. Do you want to reboot now? (y/n)"
+read -r REBOOT
 
-#Add universe repository and install subversio
+if [[ "$REBOOT" == "y" || "$REBOOT" == "Y" ]]; then
+  sudo reboot
+else
+  echo "Continuing without reboot..."
+fi
+
+# Install Asterisk 20 LTS dependencies
+sudo apt -y install git curl wget libnewt-dev libssl-dev libncurses5-dev subversion libsqlite3-dev build-essential libjansson-dev libxml2-dev uuid-dev
+
+# Add universe repository and install subversion
 sudo add-apt-repository universe
 sudo apt update && sudo apt -y install subversion
 
-#Download Asterisk 20 LTS tarball
-# sudo apt policy asterisk
+# Download Asterisk 20 LTS tarball
 cd /usr/src/
 sudo curl -O http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
 
-#Extract the file
+# Extract the file
 sudo tar xvf asterisk-20-current.tar.gz
 cd asterisk-20*/
 
-#download the mp3 decoder library
+# Download the mp3 decoder library
 sudo contrib/scripts/get_mp3_source.sh
 
-#Ensure all dependencies are resolved
+# Ensure all dependencies are resolved
 sudo contrib/scripts/install_prereq install
 
-#Run the configure script to satisfy build dependencies
+# Run the configure script to satisfy build dependencies
 sudo ./configure
 
-#Setup menu options by running the following command:
-sudo make menuselect
+# Setup menu options by running the following command:
+sudo make menuselect.makeopts
+sudo menuselect/menuselect --enable app_macro menuselect.makeopts
 
-#On Add-ons select chan_ooh323 and format_mp3 .
-#On Core Sound Packages, select the formats of Audio packets. Music On Hold, select 'Music onhold file package'
-# select Extra Sound Packages
-#Enable app_macro under Applications menu
-#Change other configurations as required
+# (Manual step: Select chan_ooh323, format_mp3, and other options as needed)
 
-#Build Asterisk
+# Build Asterisk
 sudo make
 
-#Install Asterisk by running the command:
+# Install Asterisk
 sudo make install
 
-#Install documentation(Optionally)
-sudo make progdocs
+# Optionally install documentation
+# sudo make progdocs
 
-#Install configs and samples
+# Install configs and samples
 sudo make samples
 sudo make config
 
-#Create a separate user and group to run asterisk services:
+# Create a separate user and group to run Asterisk services:
 sudo groupadd asterisk
 sudo useradd -r -d /var/lib/asterisk -g asterisk asterisk
 sudo usermod -aG audio,dialout asterisk
@@ -64,25 +68,22 @@ sudo chown -R asterisk.asterisk /etc/asterisk
 sudo chown -R asterisk.asterisk /var/{lib,log,spool}/asterisk
 sudo chown -R asterisk.asterisk /usr/lib/asterisk
 
-#Set Asterisk default user to asterisk:
-sudo vim /etc/default/asterisk
+# Set Asterisk default user to 'asterisk':
+# sudo sed -i 's/^AST_USER=.*/AST_USER="asterisk"/' /etc/default/asterisk
+# sudo sed -i 's/^AST_GROUP=.*/AST_GROUP="asterisk"/' /etc/default/asterisk
 
-# AST_USER="asterisk"
-# AST_GROUP="asterisk"
-# sudo ldconfig
+# Configure Asterisk to run as 'asterisk' user and group:
+sudo sed -i 's/^runuser=.*/runuser=asterisk/' /etc/asterisk/asterisk.conf
+sudo sed -i 's/^rungroup=.*/rungroup=asterisk/' /etc/asterisk/asterisk.conf
 
-sudo vim /etc/asterisk/asterisk.conf
-# runuser = asterisk ; The user to run as.
-# rungroup = asterisk ; The group to run as.
-
-#Restart asterisk service
+# Restart Asterisk service
 sudo systemctl restart asterisk
 
-#Enable asterisk service to start on system  boot
+# Enable Asterisk service to start on boot
 sudo systemctl enable asterisk
 
-#Test to see if it connect to Asterisk CLI
+# Test connection to Asterisk CLI
 sudo asterisk -rvv
 
-#open http ports and ports 5060,5061 in ufw firewall
-sudo ufw allow proto tcp from any to any port 5060,5061
+# Open HTTP ports and SIP ports 5060,5061 in UFW firewall
+# sudo ufw allow proto tcp from any to any port 5060,5061
